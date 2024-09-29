8ab6f7471bfda237bb888bbd1b9d03f469887559
Aditi2603 <adibwn@gmail.com>
2024-09-29 17:58:16 -0400

    change part 3

--- Exercise2_BatteryLifePrediction/+helper/displayFeatures.m
+++ Exercise2_BatteryLifePrediction/+helper/displayFeatures.m
@@ -1,13 +1,17 @@
 function displayFeatures(XTrain)
-    % Define the list of features to display
-    selectedFeatures = {
-        'DeltaQ_min', ...
-        'Qd2', ...
-        'AvgChargeTime', ...
-        'MinIR', ...
-        'IRDiff2And100'
-    };
 
-    % Display the first few rows of the selected features
-    disp(head(XTrain(:, selectedFeatures)));
+    % Define the original feature names and their corresponding understandable names
+    originalFeatureNames = {'DeltaQ_min', 'Qd2', 'AvgChargeTime', 'MinIR'};
+    understandableNames = {'Discharge Voltage Difference', 'Initial Discharge Capacity', 'Average Charge Time', ...
+         'Minimum Internal Resistance'};
+
+    
+    % Extract the selected features using the original order
+    XSelected = XTrain(:, originalFeatureNames);
+
+    % Rename the features using the understandable names in the specified order
+    XSelected.Properties.VariableNames = understandableNames;
+
+    % Display the first few rows of the renamed features
+    disp(head(XSelected));
 end
